f24f174cfabebda71d32e608b0ccf2d1329a9c02
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-13 16:49:30 -0400

    feat: shrink container

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,20 @@
-import Navbar from './Navbar';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Homepage from './Homepage';
-import RegisterForm from './RegisterForm';
 import Container from '@material-ui/core/Container';
+
+import Homepage from './Homepage';
+import Navbar from './Navbar';
+import RegisterForm from './RegisterForm';
+import InviteForm from './InviteForm';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
-        <Container maxWidth="lg" style={{ padding: '1em 0 1em 0' }}>
+        <Container maxWidth="md" style={{ padding: '1em 0 1em 0' }}>
           <Route path="/" exact component={Homepage} />
           <Route path="/register" exact component={RegisterForm} />
-          <Route path="/invite" exact component={Homepage} />
+          <Route path="/invite" exact component={InviteForm} />
           <Route path="/ranking" exact component={Homepage} />
         </Container>
       </BrowserRouter>
