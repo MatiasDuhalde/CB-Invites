b96e3fd08f943ec02e5171b65b70d06c6c9d7228
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-13 14:00:56 -0400

    refactor: use grid component in appbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,12 +3,26 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 import logo from '../assets/logo.svg';
 
 const styles = theme => ({
   logoImg: {
-    padding: '0 8px',
+    marginTop: '10px',
+  },
+  title: {
+    textAlign: 'center',
+  },
+  gridStyle: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'nowrap',
+  },
+  titleContainer: {
+    flexGrow: 1,
   },
 });
 
@@ -20,8 +34,16 @@ class Navbar extends React.Component {
       <React.Fragment>
         <AppBar position="fixed">
           <Toolbar>
-            <img className={classes.logoImg} src={logo} alt="logo" />
-            <Typography variant="h4">CurrencyBird</Typography>
+            <Grid container className={classes.gridStyle} spacing={2}>
+              <Grid item>
+                <img className={classes.logoImg} src={logo} alt="logo" />
+              </Grid>
+              <Grid item className={classes.titleContainer}>
+                <Typography className={classes.title} variant="h4">
+                  App de Invitaciones
+                </Typography>
+              </Grid>
+            </Grid>
           </Toolbar>
         </AppBar>
         <Toolbar />
