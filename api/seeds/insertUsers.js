7224eed64e637d5884dcb2a5b565cd3b34a8efe1
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-13 21:31:14 -0400

    feat: better seeds with faker

--- api/seeds/insertUsers.js
+++ api/seeds/insertUsers.js
@@ -1,25 +1,28 @@
 const pool = require('../config/pool');
+const faker = require('faker');
 
-const exampleUsers = [
-  {
-    fullName: 'John Doe',
-    email: 'johndoe@gmail.com',
-    sex: 1,
-    address: 'Santiago',
-  },
-  {
-    fullName: 'Juan Pérez',
-    email: 'juanperez@yahoo.es',
-    sex: 1,
-    address: 'Valparaíso',
-  },
-  {
-    fullName: 'Jane Doe',
-    email: 'janedoe@yahoo.es',
-    sex: 2,
-    address: 'Puerto Varas',
-  },
-];
+const generateRandomUser = () => {
+  const randomDate = faker.date.between('2010-04-30', '2021-04-30');
+
+  return {
+    email: faker.internet.email(),
+    fullName: faker.name.findName(),
+    sex: Math.floor(Math.random() * 3),
+    address: `${faker.address.streetAddress(
+      Math.floor(Math.random() * 2),
+    )}, ${faker.address.city()}, ${faker.address.country()}`,
+  };
+};
+
+const generateRandomUsers = (n = 1) => {
+  const users = [];
+  for (let i = 0; i < n; i += 1) {
+    users.push(generateRandomUser());
+  }
+  return users;
+};
+
+const exampleUsers = generateRandomUsers(100);
 
 const insertUsers = async users => {
   try {
