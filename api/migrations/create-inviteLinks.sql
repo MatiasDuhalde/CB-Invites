7ed450edaf5b419208f5ad599d7cc4a741d89ddf
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-13 20:26:47 -0400

    feat: capitalize attribs

--- api/migrations/create-inviteLinks.sql
+++ api/migrations/create-inviteLinks.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS inviteLinks CASCADE;
 
-CREATE TABLE inviteLinks(
+CREATE TABLE "inviteLinks"(
     id SERIAL PRIMARY KEY,
-    userId INT REFERENCES users(id),
+    "userId" INT REFERENCES users(id),
     code VARCHAR(255) NOT NULL UNIQUE,
-    createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT NOW()
 )
--- api/migrations/create-invites.sql
+++ api/migrations/create-invites.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS invites CASCADE;
 
 CREATE TABLE invites(
-    inviterId INT REFERENCES users(id),
-    invitedId INT REFERENCES users(id),
-    inviteId INT REFERENCES users(id),
-    PRIMARY KEY(inviterId, invitedId)
+    "inviterId" INT REFERENCES users(id),
+    "invitedId" INT REFERENCES users(id),
+    "inviteId" INT REFERENCES users(id),
+    PRIMARY KEY("inviterId", "invitedId")
 )
