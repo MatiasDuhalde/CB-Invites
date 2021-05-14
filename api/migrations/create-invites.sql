23000adedd6c0a9e882cae6e69deec0faf140943
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-13 23:27:18 -0400

    fix: fix model typos

--- api/migrations/create-invites.sql
+++ api/migrations/create-invites.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS invites CASCADE;
 
 CREATE TABLE invites(
     "inviterId" INT REFERENCES users(id),
-    "invitedId" INT REFERENCES users(id),
-    "inviteId" INT REFERENCES users(id),
+    "invitedId" INT REFERENCES users(id) UNIQUE,
+    "inviteLinkId" INT REFERENCES "inviteLinks"(id),
     PRIMARY KEY("inviterId", "invitedId")
 )
