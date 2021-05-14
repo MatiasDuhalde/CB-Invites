fcebcd9a965d68b67fa11211c9e35ba9eda8c0f2
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-14 04:07:21 -0400

    fix: add cors middleware to fix requests

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -7,11 +8,12 @@ const invitesRouter = require('./routes/invitesRouter');
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 
 // Routers
 app.use(invitesRouter);
 
-app.listen(3000, () => {
+app.listen(3005, () => {
   console.log('Listening...');
 });
