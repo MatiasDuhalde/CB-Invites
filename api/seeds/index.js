d37449778be120ee22aef21d7c77d8cb6a5ab47e
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-15 03:16:19 -0400

    fix: fix asynchronous execution of seeds

--- api/seeds/index.js
+++ api/seeds/index.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-const basename = path.basename(module.filename);
-
-console.log('Running seeds...');
-
-fs.readdirSync(__dirname)
-  .filter(
-    file =>
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js',
-  )
-  .forEach(file => {
+(async () => {
+  const basename = path.basename(module.filename);
+  console.log('Running seeds...');
+  const files = fs
+    .readdirSync(__dirname)
+    .filter(
+      file =>
+        file.indexOf('.') !== 0 &&
+        file !== basename &&
+        file.slice(-3) === '.js',
+    );
+  for (file of files) {
     const filePath = path.join(__dirname, file);
     console.log(`Running seed: ${filePath}`);
     await require(filePath)();
-  });
-
-console.log('All seeds done');
+  }
+  console.log('All seeds done');
+})();
