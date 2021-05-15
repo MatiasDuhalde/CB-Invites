5a269cc2bad0074f520b0dc288287df7c9cc2572
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-15 03:01:00 -0400

    fix: make seeds execution synchronous

--- api/seeds/index.js
+++ api/seeds/index.js
@@ -5,20 +5,15 @@ const basename = path.basename(module.filename);
 
 console.log('Running seeds...');
 
-Promise.all(
-  fs
-    .readdirSync(__dirname)
-    .filter(
-      file =>
-        file.indexOf('.') !== 0 &&
-        file !== basename &&
-        file.slice(-3) === '.js',
-    )
-    .map(file => {
-      const filePath = path.join(__dirname, file);
-      console.log(`Running seed: ${filePath}`);
-      return require(filePath)();
-    }),
-).then(() => {
-  console.log('All seeds done');
-});
+fs.readdirSync(__dirname)
+  .filter(
+    file =>
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js',
+  )
+  .forEach(file => {
+    const filePath = path.join(__dirname, file);
+    console.log(`Running seed: ${filePath}`);
+    await require(filePath)();
+  });
+
+console.log('All seeds done');
